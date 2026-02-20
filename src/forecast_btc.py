44cdd093885ce85a099894cca88c9f966e6bc916
Monika Etrych <metrych@griddynamics.com>
2026-02-20 11:02:57 +0100

    Yahoo Finance data retriever
    volume from all markets

--- src/forecast_btc.py
+++ src/forecast_btc.py
@@ -1,14 +1,17 @@
-import joblib
-import requests
-import pandas as pd
-import numpy as np
-import time
-from .data_processor import transform_data
-# Add the project root to Python path
 import sys
 from pathlib import Path
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
+
+from src.config import COLUMN_TO_PREDICT
+import joblib
+import yfinance as yf
+import requests
+import pandas as pd
+import numpy as np
+import time
+from src.data_processor import transform_data
+
 
 def count_days_since_last_candle(df=None):
     """Counts days since last candle till today."""
@@ -21,36 +24,21 @@ def count_days_since_last_candle(df=None):
     return days_since_last_candle
 
 
-def get_crypto_data(symbol="BTCUSDT", interval="1d", limit=500):
-    """Downloads OHLCV data from Binance.US API for a given cryptocurrency symbol, time interval, and data limit."""
-    base_url = "https://api.binance.us/api/v3/klines"
-    params = {
-        "symbol": symbol,
-        "interval": interval,
-        "limit": limit+1
-    }
-    
-    response = requests.get(base_url, params=params)
-    
-    if response.status_code != 200:
-        print("Error during downloading data:", response.text)
-        return None
+def get_crypto_data_yahoo(symbol="BTC-USD", interval="1d", limit=500):
+    """Downloads OHLCV data from Yahoo Finance API for a given cryptocurrency symbol, time interval, and data limit."""
+    limit += 1
+    df = yf.download(symbol, period=f"{limit}d", interval=interval)
+    df = df.reset_index()
 
-    data = response.json()
-    
-    df = pd.DataFrame(data, columns=[
-        'Date', 'Open', 'High', 'Low', 'Close', 'Volume',
-        'Close_Time', 'Quote_Volume', 'Trades', 'Takers_Buy_Base', 'Takers_Buy_Quote', 'Ignore'
-    ])
+    # Handle MultiIndex columns if present
+    if isinstance(df.columns, pd.MultiIndex):
+        df.columns = df.columns.get_level_values(0)
 
-    # Choose only columns 0-5 (Date, O, H, L, C, V)
-    df = pd.DataFrame(data).iloc[:, range(6)]
-    df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-
-    numeric_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
-    df[numeric_cols] = df[numeric_cols].astype(float)
+    df = df[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
 
     df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+    numeric_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
+    df[numeric_cols] = df[numeric_cols].astype(float)
 
     return df
 
@@ -62,11 +50,11 @@ def run_production_inference(model_path: str, pipeline_path: str):
 
     # 2. Get latest data
     days_to_get = count_days_since_last_candle()
-    df_live = get_crypto_data(symbol="BTCUSDT", interval="1d", limit=days_to_get)
+    df_live = get_crypto_data_yahoo(interval="1d", limit=days_to_get)
     if df_live is None or df_live.empty:
         print("Error: Failed to retrieve data from the API.")
         return None
-    
+        
     # 3. Transform data using the same pipeline as training
     try:
         _, _, X_live, _, _  = transform_data(df_live, test=None, pipeline=pipeline, verbose=False)
@@ -74,16 +62,19 @@ def run_production_inference(model_path: str, pipeline_path: str):
         print(f"Error during data transformation: {e}")
         return None
 
+    print (X_live.head())
     # 5. Make prediction
     prediction = model.predict(X_live)
 
     # 6. Result
     current_date = df_live['Date'].iloc[-1]
     print("-" * 30)
-    print(f"PREDICTION RAPORT FOR: BTCUSDT")
+    print(f"PREDICTION RAPORT FOR: BTC-USD")
     print(f"Date of last candle: {current_date}")
     print(f"Predicted volatility for tomorrow: {prediction[0]:.4f}")
     print("-" * 30)
     
     return prediction[0]
 
+
+run_production_inference(model_path=f'models/{COLUMN_TO_PREDICT}/best_model.pkl', pipeline_path=f'models/{COLUMN_TO_PREDICT}/feature_pipeline.pkl')
